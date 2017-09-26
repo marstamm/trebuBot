37133dc7e2af72d11fa393c68b43fc9fcd6510a5
Martin Stamm <martin.stamm@student.hpi.de>
2017-09-26 13:17:45 +0200

    fix: JSON parsing now done by Amazon API

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ var https = require('https');
 var TelegramBot = require('node-telegram-bot-api');
 
 // Telegram bot token (given when you create a new bot using the BotFather);
-const BotToken = require('./token');
+var BotToken = "465451620:AAFw87x9es0SPKM05rZ_KaRe9k6sL3MMDIk";
 
 // Telegram bot setup
 var telegramBot = new TelegramBot(BotToken, {polling: false});
 
 exports.handler = function(event, context, lambdaCallback) {
     // parse the chat ID so we can respond
-    var chatId = JSON.parse(event.body).message.chat.id;
+
+    var chatId = event.body.message.chat.id;
 
     // let them know we're working
     telegramBot.sendMessage(chatId, "I ain't doing shit");
 
-};
+}
