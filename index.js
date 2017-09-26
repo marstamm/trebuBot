6614ecd0aa9a90207712e9acd038d5e66a8a4356
Martin Stamm <martin.stamm@student.hpi.de>
2017-09-26 13:36:31 +0200

    Add XD handling

--- index.js
+++ index.js
@@ -8,17 +8,45 @@ var https = require('https');
 var TelegramBot = require('node-telegram-bot-api');
 
 // Telegram bot token (given when you create a new bot using the BotFather);
-var BotToken = "465451620:AAFw87x9es0SPKM05rZ_KaRe9k6sL3MMDIk";
+const BotToken = require('./token');
 
 // Telegram bot setup
 var telegramBot = new TelegramBot(BotToken, {polling: false});
 
 exports.handler = function(event, context, lambdaCallback) {
+
     // parse the chat ID so we can respond
+    let chatId;
+    if (event.body.message && event.body.message.chat && event.body.message.chat.id) {
+      chatId = event.body.message.chat.id;
+    } else if (event.body.channel_post && event.body.channel_post.chat && event.body.channel_post.chat.id) {
+      chatId = event.body.channel_post.chat.id;
+    }
 
-    var chatId = event.body.message.chat.id;
+    //parse message
+    let message;
+    if (event.body.channel_post && event.body.channel_post.text) {
+      message = event.body.channel_post.text;
+    } else if (event.body.message && event.body.message.text) {
+      message = event.body.message.text;
+    }
 
-    // let them know we're working
-    telegramBot.sendMessage(chatId, "I ain't doing shit");
 
+    let reply;
+    if(message.toLowerCase().includes("xd"))
+    {
+      var xd = ['x', 'd', 'X', 'D'];
+      //Topkek bot already handles these
+      if(!message.includes("xD"))
+      {
+        reply = "x";
+        for(var i = 0; i < Math.random()*10; i++)
+        {
+          reply += xd[Math.random()*3];
+        }
+        reply += "D";
+
+        telegramBot.sendMessage(chatId, reply);
+      }
+    }
 }
